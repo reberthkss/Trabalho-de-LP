a20356a18d9734c86c2a55f50b184a1990ba587e
Reberth <reberth.santos@unifesp.br>
2019-09-09 17:04:31 -0300

    Funcoes separadas da main

---
+++ funcoes.c
@@ -0,0 +1,13 @@
+#include "funcoes.h"
+
+
+void limparvetor(int *v1,int *v2){
+
+int i;
+
+for(i=0;i<23;i++){
+    *(v1+i)=0;
+    *(v2+i)=0;
+}
+}
+
---
+++ main.c
@@ -0,0 +1,34 @@
+
+#include<stdio.h>
+#include<math.h>
+#include <string.h>
+#include "funcoes.h"
+
+
+int assentoC[23];
+int assentoJ[23];
+float valort=0;
+
+
+
+int main(){
+
+int gameover=0;
+int opt;
+int i=0;
+
+
+    limparvetor(&assentoC,&assentoJ);
+
+while(!gameover){
+// MENU PRINCIPAL
+// -- func menuprincipal()
+acao(opt=menuprincipal(),&gameover);
+}
+
+
+return 0;
+
+}
+
+
